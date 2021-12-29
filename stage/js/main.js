2fe0d04b42cbeb1843cd7d001d8f6d8d1d749231
abedulaziz <abdulaziz.kharraz@outlook.com>
2021-12-29 18:32:22 +0200

    commit 5

--- stage/js/main.js
+++ stage/js/main.js
@@ -7,4 +7,35 @@ $(function() {
         $(this).toggleClass("fa-angle-right fa-angle-down");
         $(this).parent('a').next('.child-links').slideToggle();
     })
+
+    // open/close fullscreen
+    $(".toggle-fullscreen").on("click", function() {
+        $(this).toggleClass("full-screen");
+        if ($(this).hasClass("full-screen")) {
+            openFullscreen();
+        }else closeFullscreen();
+    })
 })
+
+var elem = document.documentElement;
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
